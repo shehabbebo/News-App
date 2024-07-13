0bb19f3eee8bc5b491e083e21fec097722806e64
shehabbebo <shehabbebo27@gmail.com>
2024-07-13 09:45:01 +0300

    first commit

---
+++ lib/services/news_service.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:news_app_ui_setup/models/article_model.dart';
+
+class NewsService {
+  final Dio dio;
+
+  NewsService(this.dio);
+
+  Future<List<ArticleModel>> getTopHeadlines({required String category}) async {
+    try {
+      var response = await dio.get(
+          'https://newsapi.org/v2/top-headlines?country=us&apiKey=0c5512db4f7d4ccc9dd675cd64819646&category=$category');
+      //'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=0c5512db4f7d4ccc9dd675cd64819646');
+      Map<String, dynamic> jsonData = response.data;
+      //Map<String, dynamic> jsonData = response.data;
+
+      List<dynamic> articles = jsonData['articles'];
+
+      List<ArticleModel> articlesList = [];
+
+      for (var article in articles) {
+        ArticleModel articleModel = ArticleModel.fromJson(article);
+        articlesList.add(articleModel);
+      }
+
+      return articlesList;
+    } catch (e) {
+      return [];
+    }
+  }
+}
